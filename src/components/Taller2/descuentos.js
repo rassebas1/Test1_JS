9ab6e8bc5628a0342ebc5217d15387b84e50639b
rassebas1 <sebastian.espitia@4coders.co>
2022-04-08 16:51:34 -0500

    CalculaDescuentos

--- src/components/Taller2/descuentos.js
+++ src/components/Taller2/descuentos.js
@@ -1,2 +1,8 @@
-var discount;
-var price;
+function discountedPrice() {
+  var discount = Number(document.getElementById("input-discount").value);
+  var price = Number(document.getElementById("input-price").value);
+  console.log(price * ((100 - discount) / 100));
+  document.getElementById("result-span").textContent =
+    price * ((100 - discount) / 100);
+  return price * ((100 - discount) / 100);
+}
